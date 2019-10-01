94d4eb214b8ae07d189bc8cbb6920ec34d4f78bd
Wills Jarquin <jarquinwills@gmail.com>
2019-10-01 21:21:49 +0000

    We added a post nitification and now are ready to just fix the order

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,16 @@ class TasksController < ApplicationController
     render json: Task.all
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(tasks_params)
+    render json: task
+  end
+
+  private 
+
+  def tasks_params
+    params.require(:task).permit(:done)
+  end
+
 end
